3b5c924b656e0f25179cf41e5bc1fce551233eb5
MrDog210 <tusakf@gmail.com>
2024-03-29 11:10:00 +0100

    added basic logic

--- src/pages/TimetablePage.js
+++ src/pages/TimetablePage.js
@@ -1,32 +1,42 @@
-import { useState } from "react"
-import { getStoredSchoolCode } from "../util/webStorage"
+import { useEffect, useState } from "react"
+import { getSelectedProgramYearAndBranch, getStoredSchoolCode } from "../util/webStorage"
 import { Calendar, momentLocalizer } from "react-big-calendar"
 import moment from "moment"
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import { fetchGroupsForBranch, fetchLecturesForGroups } from "../util/http";
+import { getWeekDates } from "../util/dateUtils";
 
 function TimetablePage() {
   const [schoolCode] = useState(getStoredSchoolCode())
-  const [lectures, setLectures] = useState([{
-    start: moment().toDate(),
-    end: moment()
-      .add(3, "hours")
-      .toDate(),
-    title: "Some title"
-  },
-  {
-    start: moment().toDate(),
-    end: moment()
-      .add(3, "hours")
-      .toDate(),
-    title: "Some title 2"
-  }])
+  const [lectures, setLectures] = useState([])
+  const [date, setDate] = useState(new Date())
+  
+  useEffect(() => {
+    async function refreshLectures() {
+      const info = getSelectedProgramYearAndBranch()
+      console.log(info)
+      const allGroups = await fetchGroupsForBranch(schoolCode, info.branch.id)
+      const {from, till} = getWeekDates(date)
+      const data = await fetchLecturesForGroups(schoolCode, allGroups, from, till)
+      data.forEach(lecture => {
+        lecture.start_time = new Date(lecture.start_time)
+        lecture.end_time = new Date(lecture.end_time)
+      });
+      console.log(data)
+      setLectures(data)
+    }
+    refreshLectures()
+  }, [date])
+  
   const localizer = momentLocalizer(moment)
   return (
     <Calendar 
       localizer={localizer}
       events={lectures}
-      startAccessor="start"
-      endAccessor="end"
+      startAccessor="start_time"
+      endAccessor="end_time"
+      defaultView="work_week"
+      views={["work_week"]}
       style={{ height: 500 }}
     />
   )
