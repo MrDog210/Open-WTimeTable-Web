28d2d28e89ecad71b105b41205a079a41162385b
MrDog210 <tusakf@gmail.com>
2025-10-03 13:16:41 +0200

    Create basic code input screen

--- src/app/pages/setup/SchoolCodeInputPage.tsx
+++ src/app/pages/setup/SchoolCodeInputPage.tsx
@@ -1,9 +1,16 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 function SchoolCodeInputPage() {
 
   return (
-    <div>
-      
+    <div className="flex h-screen flex-col justify-center items-center gap-4 ">
+      <form className="flex flex-col gap-4" onSubmit={(e) => {
+        e.preventDefault()
+      }}>
+        <Input minLength={1} type="text" placeholder="Enter your school code (feri)" className="w-72" />
+        <Button type="submit" className="w-72">Submit</Button>
+      </form>
     </div>
   )
 }
